81d0ee614aa72ad3560902e02f48972ed33787a8
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-10-12 02:35:22 +0300

    2nd commit

--- server/app.py
+++ server/app.py
@@ -23,6 +23,65 @@ def bakeries():
     bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
     return make_response(  bakeries,   200  )
 
+@app.route('/bakeries/<int:id>',methods=['GET','PATCH'])
+def bakeries_by_id(id):
+    bakery=Bakery.query.filter(Bakery.id==id).first()
+    
+    if request.method=="GET":
+        bakery_dict=bakery.to_dict()
+        return make_response(bakery_dict,200)
+    
+    elif request.method=='PATCH':
+        for attr in request.form:
+            setattr(bakery,attr,request.form.get(attr))
+        db.session.add(bakery)
+        db.session.commit()
+        bakery_dict=bakery.to_dict()
+        
+        return make_response(bakery_dict,200)
+        
+
+
+@app.route('/baked_goods',methods=['GET','POST'])
+def baked_goods():
+    bakedgoods=BakedGood.query.all()
+    
+    if request.method=="GET":
+        baked=[good.to_dict() for good in bakedgoods]
+        return make_response(baked,200)
+    
+    elif request.method=="POST":
+        new_good=BakedGood(
+            name=request.form.get('name'),
+            price=request.form.get('price'),
+            bakery_id=request.form.get('bakery_id')     
+        )
+        db.session.add(new_good)
+        db.session.commit()
+        
+        new_good_dict=new_good.to_dict()
+        
+        return make_response(new_good_dict,201)
+
+@app.route('/baked_goods/<int:id>',methods=['GET','DELETE'])
+def baked_goods_by_id(id):
+    bakedgood=BakedGood.query.filter(BakedGood.id==id).first()
+    
+    if request.method=="GET":
+        bakedgood_dict=bakedgood.to_dict()
+        return make_response(bakedgood_dict,200)
+    
+    elif request.method=="DELETE":
+        
+        db.session.delete(bakedgood)
+        db.session.commit()
+        
+        response_body={
+            "message":"record successfully deleted"
+        }
+        
+        return make_response(response_body,200)
+
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
 
